2fdd84367ebf0b11add7b6d2e16e98931695bc76
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-05-18 13:37:28 -0400

    lecture work-in-progress

--- client/src/components/GasPrices.js
+++ client/src/components/GasPrices.js
@@ -18,6 +18,7 @@ class GasPrices extends React.Component {
       .get('http://localhost:5000/api/data')
       .then(res=>{
         this.setState({
+          ...this.state,
           gasPrices: res.data.data
         });
       })
